a0aa3be7809b1ff1dda50115cf9e7947321fb9fa
samehhamdyroqay <82160791+samehhamdyroqay@users.noreply.github.com>
2024-04-02 13:28:07 +0200

    feat set localization header for mobile apis

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -80,5 +80,6 @@ class Kernel extends HttpKernel
         'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
         'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
         'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+        'locale'                => \App\Http\Middleware\Localization::class,
     ];
 }
--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
 
 class Localization
@@ -11,15 +13,18 @@ class Localization
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param  \Closure(Request): (Response|RedirectResponse)  $next
+     * @return Response|RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
         /* Set new lang with the use of session */
         if (session()->has('lang')) {
-            App::setLocale(session()->get('lang'));
+            App::setLocale(session('lang'));
+        }
+        if ($request->header('X-localization')) {
+            App::setLocale($request->header('X-localization'));
         }
         return $next($request);
     }
