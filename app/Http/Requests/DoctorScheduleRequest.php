0fe94c2ea6827579a42e0bf61be8233a2edf16f6
samehhamdyroqay <82160791+samehhamdyroqay@users.noreply.github.com>
2024-07-06 18:17:04 +0300

    feat add doctor check on verify

--- app/Http/Requests/DoctorScheduleRequest.php
+++ app/Http/Requests/DoctorScheduleRequest.php
@@ -21,16 +21,18 @@ class DoctorScheduleRequest extends FormRequest
 
     public static function afterValidation($validated)
     {
-        $validated['schedule_days'] = collect(CarbonPeriod::between(request('schedule_repeat_from'), request('schedule_repeat_to')))->map(function ($date) {
-            $dayName = strtolower($date->format('l'));
-            if (in_array($dayName, array_column(request('schedule_days'), 'day'))) {
-                return [
-                    'date' => $date->format('Y-m-d'),
-                    'shifts' => collect(request('schedule_days'))->firstWhere('day', $dayName)['shifts'],
-                ];
-            }
-            return null;
-        })->whereNotNull()->values()->toArray();
+        if (!empty($validated['schedule_days'])) {
+            $validated['schedule_days'] = collect(CarbonPeriod::between(request('schedule_repeat_from'), request('schedule_repeat_to')))->map(function ($date) {
+                $dayName = strtolower($date->format('l'));
+                if (in_array($dayName, array_column(request('schedule_days'), 'day'))) {
+                    return [
+                        'date' => $date->format('Y-m-d'),
+                        'shifts' => collect(request('schedule_days'))->firstWhere('day', $dayName)['shifts'],
+                    ];
+                }
+                return null;
+            })->whereNotNull()->values()->toArray();
+        }
         $validated['urgent_consultation_price'] = $validated['price'];
         $validated['with_appointment_consultation_price'] = $validated['price'];
         unset($validated['price']);
@@ -40,14 +42,14 @@ class DoctorScheduleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'consultation_period' => config('validations.integer.req'). '|min:5|max:300',
-            'schedule_days' => config('validations.array.req'),
+            'consultation_period' => config('validations.integer.null'). '|min:5|max:300',
+            'schedule_days' => config('validations.array.null'),
             'schedule_days.*.day' => config('validations.day.req'),
             'schedule_days.*.shifts' => config('validations.array.req'),
             'schedule_days.*.shifts.*.from_time' => config('validations.time.req'),
             'schedule_days.*.shifts.*.to_time' => config('validations.time.req'),
-            'schedule_repeat_from' => config('validations.date.req').'|after_or_equal:today',
-            'schedule_repeat_to' => config('validations.date.req').'|after:schedule_repeat_from',
+            'schedule_repeat_from' => config('validations.date.null').'|after_or_equal:today',
+            'schedule_repeat_to' => config('validations.date.null').'|after:schedule_repeat_from',
             'reminder_before_consultation' => config('validations.integer.req'),
             'price' => config('validations.integer.req')
         ];
