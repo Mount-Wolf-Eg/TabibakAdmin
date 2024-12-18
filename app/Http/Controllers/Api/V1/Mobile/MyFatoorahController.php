b5e9dbcfbb5ed2371b8f72ba801e6508100b0dab
Elsayed Feteh <elsayed.feteh@yahoo.com>
2024-12-18 23:04:39 +0200

    handle Consultation notification

--- app/Http/Controllers/Api/V1/Mobile/MyFatoorahController.php
+++ app/Http/Controllers/Api/V1/Mobile/MyFatoorahController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\V1\Mobile;
 use App\Constants\PaymentStatusConstants;
 use App\Http\Controllers\Controller;
 use App\Models\Consultation;
-use App\Services\Repositories\ConsultationNotificationService;
 use App\Traits\BaseApiResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -19,8 +18,6 @@ use Exception;
 class MyFatoorahController extends Controller
 {
     use BaseApiResponseTrait;
-
-    private ConsultationNotificationService $notificationService;
     
     /**
      * @var array
@@ -30,14 +27,12 @@ class MyFatoorahController extends Controller
     /**
      * Initiate MyFatoorah Configuration
      */
-    public function __construct(ConsultationNotificationService $notificationService) {
+    public function __construct() {
         $this->mfConfig = [
             'apiKey'      => config('myfatoorah.api_key'),
             'isTest'      => config('myfatoorah.test_mode'),
             'countryCode' => config('myfatoorah.country_iso'),
         ];
-
-        $this->notificationService = $notificationService;
     }
 
     /**
@@ -115,7 +110,6 @@ class MyFatoorahController extends Controller
                 $order = Consultation::withoutGlobalScope('isActive')->where('id', $data->CustomerReference)->first();
                 $order?->update(['is_active' => true]);
                 $order?->payment()->update(['transaction_id' => $paymentId, 'status' => PaymentStatusConstants::CANCELLED->value]);
-                $this->notificationService->newConsultation($order);
             } else {
                 info(json_encode($data));
             }
