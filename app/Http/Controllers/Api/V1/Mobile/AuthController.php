996c5c1a806db36c3ccf1b4e443facfa7e786bbb
Elsayed Feteh <elsayed.feteh@yahoo.com>
2024-12-31 19:15:11 +0200

    Handle fallback verification code in local environment for testing purposes

--- app/Http/Controllers/Api/V1/Mobile/AuthController.php
+++ app/Http/Controllers/Api/V1/Mobile/AuthController.php
@@ -59,6 +59,7 @@ class AuthController extends BaseApiController
         if (env('APP_ENV') == 'local' && $request->verification_code == 1234 && !$loginUser) {
             $loginUser = $this->contract->findByFields(['and' => ['phone' => $request->phone, 'is_active' => true]]); // for testing remove this line in production
             info($request->verification_code);
+            info($request->phone);
             info($loginUser);
         }
         if ($loginUser && $loginUser->is_active) {
