539e6e645c15a8c4d3b8b815fe650f8688eeac13
Elsayed Feteh <elsayed.feteh@yahoo.com>
2025-01-01 23:15:53 +0200

    return is_active

--- app/Models/User.php
+++ app/Models/User.php
@@ -88,7 +88,7 @@ class User extends Authenticatable
         return $role ? $role->id : null;
     }
 
-    public function getDoctorIsActivesAttribute()
+    public function getDoctorIsActiveAttribute()
     {
         return $this->is_active && $this->doctor?->is_active && $this->doctor?->request_status->value == 2 ? 1 : 0;
     }
